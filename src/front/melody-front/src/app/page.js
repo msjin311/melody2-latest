ae93fc5acd077bd39a81a85fd4fafca990a44661
HvvanE <ssjjhh0511@naver.com>
2023-10-17 14:38:20 +0900

    로그인 / 로그아웃 구현
    로그인 / 로그아웃 구현

--- src/front/melody-front/src/app/page.js
+++ src/front/melody-front/src/app/page.js
@@ -2,13 +2,36 @@
 
 import Link from "next/link";
 import ClientComponent from "../components/ClientComponent";
+import {useContext} from "react";
+import {UserContext} from "../contexts/UserContext";
 
 const Home = () => {
+
+    const { userState, userDispatch } = useContext(UserContext);
+
+
     return (
         <div>
             <h1>Server Component</h1>
             <ClientComponent />
             <h1> <Link href="/login"> test </Link></h1>
+            <h1> <Link href="/addPlaylistTest"> playlist creat test </Link></h1>
+            <h1> <Link href="/addSongTest"> add song to playlist test </Link></h1>
+            <h1> <Link href="/playlistPrintTest"> playlist print test </Link></h1>
+            <h1> <Link href="/detailTest"> detail print test </Link></h1>
+
+
+            {userState.isAuthenticated ? (
+                <>
+                    <p>Welcome, {userState.user.name}!</p>
+                    <p>Welcome, {userState.user.email}!</p>
+                </>
+            ) : (
+                <>
+                    no login yet
+                </>
+            )}
+
         </div>
     );
 };
