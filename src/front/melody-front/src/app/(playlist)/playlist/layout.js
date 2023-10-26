fd05baa06d9c82d633e8c1c3e1bd2428e1aa3433
msjin311 <136568295+msjin311@users.noreply.github.com>
2023-10-26 10:02:29 +0900

    css 1차 변경
    css 1차 변경

--- src/front/melody-front/src/app/(playlist)/playlist/layout.js
+++ src/front/melody-front/src/app/(playlist)/playlist/layout.js
@@ -46,7 +46,7 @@ const Layout = ({ children }) => {
 
     return (
 
-        <div className="flex h-screen bg-gray-100 justify-content-center">
+        <div className="px-4 py-8">
             <div>
                 <div className={`menu-button ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
                     <Image src={menuImg} alt="menu image" width={50}  />
@@ -59,38 +59,9 @@ const Layout = ({ children }) => {
                 </div>
             </div>
 
-
-            {/* Sidebar */}
-            {/*<div className="w-1/4 bg-gray-800 p-6 text-white">*/}
-            {/*    /!* Sidebar content goes here *!/*/}
-            {/*    Sidebar*/}
-            {/*</div>*/}
-
             {/* Main Content */}
-            <div className="w-100 p-6">
+            <div className="px-4 py-8">
                 {/*/!* Header *!/*/}
-                {/*<header className="bg-white p-4 mb-4 shadow grid grid-cols-2">*/}
-                {/*    /!* Header content goes here *!/*/}
-                {/*    <div className="col-span-1">*/}
-                {/*        <h1><b>내 라이브러리</b></h1>*/}
-                {/*    </div>*/}
-                {/*    <div className="col-span-1 flex justify-end space-x-4">*/}
-                {/*        /!*Playlist Header*!/*/}
-                {/*        /!*<div>*!/*/}
-                {/*        /!*    <button onClick={openModal}></button>*!/*/}
-                {/*        /!*    {isModalOpen && (*!/*/}
-                {/*        /!*        <div className="modal">*!/*/}
-                {/*        /!*            <div className="modal-content">*!/*/}
-                {/*        /!*                <Link href="/createPlaylist"><Image alt="add image" src={plusImg} width={50} height={50}></Image></Link>*!/*/}
-                {/*        /!*            </div>*!/*/}
-                {/*        /!*        </div>*!/*/}
-                {/*        /!*    )}*!/*/}
-                {/*        /!*</div>*!/*/}
-                {/*        <Link href="/createPlaylist"><Image alt="noimage" src={plusImg} width={50} height={50}></Image></Link>*/}
-
-                {/*    </div>*/}
-                {/*</header>    /!*                <button onClick={closeModal}>Close Modal</button>*!/*/}
-                {/*    */}
 
                 {/* Main Content */}
                 <main className="bg-white p-4 shadow">
